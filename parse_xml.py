7870191a20823ba95299c6257c0cbb6c0d21398d
redubr <34924544+redubr@users.noreply.github.com>
2022-05-26 12:29:48 +0300

    Add files via upload

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
 
 def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
+  soup = BeautifulSoup(xml_content, 'lxml')
   
   # your code here
-  
-  return result
+  input_tag = soup.find_all("data")
+  str_p1 = str(input_tag).replace("[<data>", "")
+  value = str_p1.replace("</data>]", "")
+
+  return value.replace(" ", "").replace("\n", "").replace("\r", "")
